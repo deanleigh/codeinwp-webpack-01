11144979c987e13f6652a54a1756b5bd1b423a8f
Dean Leigh <dean.leigh@wholething.co.uk>
2021-05-11 20:44:38 +0100

    Addvariables

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    plugins: [
+        new HtmlWebpackPlugin({
+          hash: true,
+          title: 'Webpack Example App',
+          header: 'Webpack Example Title',
+          metaDesc: 'Webpack Example Description',
+          template: './src/index.html',
+          filename: 'index.html',
+          inject: 'body'
+        })
+      ]
 };
